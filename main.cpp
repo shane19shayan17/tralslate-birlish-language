5faa63afbdaf23801f7471dd1be60ab20312c7ca
shayan17shane19 <shayansetayandevolleyking5@gmail.com>
2026-01-03 15:00:26 +0330

    fix size

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 string is_reversed(string a, string b)
 {
     int str_lenght = a.size();
+    if (str_lenght != b.size())
+    {
+        return "NO";
+    }
     for (int i = 0; i < str_lenght; i++)
     {
         if (a[i] != b[str_lenght - i - 1])
